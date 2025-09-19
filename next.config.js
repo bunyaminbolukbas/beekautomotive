e0b9cf88e33686648e6f6a0cb2ae27727cfd6b2e
Bunyamin Bölükbas <bunyaminbolukbas@outlook.com>
2025-09-19 11:24:16 +0200

    next js aangepast

--- next.config.js
+++ next.config.js
@@ -1,10 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true },
+  images: { 
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
